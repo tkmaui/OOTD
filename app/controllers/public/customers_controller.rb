18103b5be4b3424cc29681382f0876966d9406e8
tkmaui <129600101+tkmaui@users.noreply.github.com>
2023-06-25 13:14:54 +0000

    mypage

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,25 +1,35 @@
 class Public::CustomersController < ApplicationController
+
   def index
     @customers = Customer.all
   end
 
   def edit
     @customer = Customer.find(params[:id])
-    
+
   end
 
   def update
     @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to customer_path(@customer.id)
   end
 
   def show
     @customer = Customer.find(params[:id])
     @outfits = @customer.outfits
+    @questions = @customer.questions
   end
 
   def destroy
   end
 
   def is_deleted
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:nickname, :profile_image, :gender, :tall, :introduction)
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,6 +11,9 @@ class Customer < ApplicationRecord
   has_many :question_favorites, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_one_attached :profile_image
+  
+  enum gender: {その他: 0 , 男性: 1, 女性: 2}
+  
   def get_profile_image
     profile_image.variant(resize_to_limit: [100, 100]).processed
   end
